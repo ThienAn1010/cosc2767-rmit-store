8baf03af2608ce19b5055873fe5913e7a03ff311
rider3458 <73817958+rider3458@users.noreply.github.com>
2022-09-08 09:24:53 +0700

    Update test_testRedirect.py

--- test_testRedirect.py
+++ test_testRedirect.py
@@ -3,19 +3,21 @@ import pytest
 import time
 import json
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class TestTestRedirect():
+class TestAppearanceCheck():
   def setup_method(self, method):
-    self.driver = webdriver.Chrome()
+    self.driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=chrome_options)
     self.vars = {}
   
   def teardown_method(self, method):
