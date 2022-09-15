2c2d134ad0b72a0a8ab407a4d60ae9bc9328eb68
rider3458 <s3878480@rmit.edu.vn>
2022-09-15 16:43:16 +0700

    fix

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
-                  "localhos",
+                  "localhost",
                   "db_admin",
                   "rmit_password",
                   "rmit_store_db"
